6d82e557ba466e635986c3f51c06b330df5858d6
Sebastian L Moran <sbssing7@gmail.com>
2023-11-01 18:47:45 -0500

    Diseño de la pantalla de busqueda

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,5 +1,42 @@
 export const SearchPage = () => {
   return (
-    <h1>SearchPage</h1>
+    <>
+      <h1>Search</h1>
+      <hr />
+
+      <div className="row">
+        <div className="col-5">
+          <h4>Searching</h4>
+          <hr />
+          <form>
+            <input 
+              type="text" 
+              placeholder="Search a Hero"
+              className="form-control"
+              name="searchText"
+              autoComplete="off"
+            />
+
+          <button className="btn btn-outline-primary mt-1">
+            Search
+          </button>
+
+          </form>
+
+        </div>
+
+        <div className="col-7">
+          <h4>Results</h4>
+          <hr />
+
+          <div className="alert alert-danger">
+            There's a no here with <b>ABC</b>
+          </div>
+
+        </div>
+      </div>
+
+      
+    </>
   )
 }
