258537c99483d9c22436a863b14a518befa77fd4
Sebastian L Moran <sbssing7@gmail.com>
2023-11-01 17:08:27 -0500

    Leer argumentos por URL

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,4 +1,14 @@
+import { Navigate, useParams } from "react-router-dom";
+import { getHeroById } from "../helpers";
+
 export const HeroPage = () => {
+  const { id } = useParams();
+  const hero = getHeroById(id);
+  if(!hero){
+    return <Navigate to="/marvel"/>
+  }
+
+
   return (
     <h1>HeroPage</h1>
   )
