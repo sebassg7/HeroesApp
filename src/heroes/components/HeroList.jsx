400f42c4af14239ce95c2216324eeb4a0a7cbeeb
Sebastian L Moran <sbssing7@gmail.com>
2023-11-01 12:49:46 -0500

    Agregar información del Héroe - Parte 1

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,4 +1,5 @@
 import { getHeroesByPublisher } from "../helpers"
+import { HeroCard } from "./HeroCard";
 
 export const HeroList = ({publisher}) => {
 
@@ -7,17 +8,17 @@ export const HeroList = ({publisher}) => {
   return (
     <>
     
-        <ul>
+        <div className=" row rows-cols-1 row-cols-md-3 g-3">
             {
-                heroes.map(heroe => {
+                heroes.map(hero => {
                     return(
-                        <li key={heroe.id}>
-                            {heroe.superhero}
-                        </li>
+                        <HeroCard 
+                            key={hero.id}
+                            {...hero} />
                     )
                 })
             }
-        </ul>
+        </div>
 
     </>
   )
