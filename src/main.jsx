653b9d25abb509c5e3e5996d44bd7c6b386b590d
sebassg7 <sebastlopez@unicauca.edu.co>
2023-12-26 15:35:32 -0500

    HashRouter Agregado

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HeroesApp } from './HeroesApp';
-import { BrowserRouter } from 'react-router-dom';
+import {  HashRouter } from 'react-router-dom';
 import './styles.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <HeroesApp />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
